5e9fb653476a2c9e3b31d61fe9c09917202f7bfc
Playlife <samuil.a.mihaylov@gmail.com>
2015-10-12 15:21:08 +0300

    Migrations added

---
+++ Authorization_App/Migrations/201510091252461_Question.cs
@@ -0,0 +1,31 @@
+namespace Authorization_App.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Question : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Questions",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        AuthorId = c.String(),
+                        Title = c.String(maxLength: 256),
+                        Description = c.String(maxLength: 512),
+                        Tags = c.String(maxLength: 1028),
+                        Level = c.Int(nullable: false),
+                        CreatedAt = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Questions");
+        }
+    }
+}
---
+++ Authorization_App/Migrations/201510091301420_QuestionOption.cs
@@ -0,0 +1,36 @@
+namespace Authorization_App.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class QuestionOption : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.QuestionOptions",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        isCorrect = c.Boolean(nullable: false),
+                        QuestionRefId = c.Int(nullable: false),
+                        Content_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Contents", t => t.Content_Id)
+                .ForeignKey("dbo.Questions", t => t.QuestionRefId, cascadeDelete: true)
+                .Index(t => t.QuestionRefId)
+                .Index(t => t.Content_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.QuestionOptions", "QuestionRefId", "dbo.Questions");
+            DropForeignKey("dbo.QuestionOptions", "Content_Id", "dbo.Contents");
+            DropIndex("dbo.QuestionOptions", new[] { "Content_Id" });
+            DropIndex("dbo.QuestionOptions", new[] { "QuestionRefId" });
+            DropTable("dbo.QuestionOptions");
+        }
+    }
+}
