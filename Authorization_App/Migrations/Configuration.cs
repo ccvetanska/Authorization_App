2b0e58bad81381c51cba9993bdc79a1481108cba
Playlife <samuil.a.mihaylov@gmail.com>
2015-10-12 17:24:21 +0300

    Default Admin user added
    default admin in Seed method added

--- Authorization_App/Migrations/Configuration.cs
+++ Authorization_App/Migrations/Configuration.cs
@@ -1,11 +1,17 @@
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using Owin;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Authorization_App.Models;
+
 namespace Authorization_App.Migrations
 {
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
-    using Authorization_App.Models;
-
     internal sealed class Configuration : DbMigrationsConfiguration<Authorization_App.Models.ApplicationDbContext>
     {
         public Configuration()
@@ -13,14 +19,32 @@ namespace Authorization_App.Migrations
             AutomaticMigrationsEnabled = true;
         }
 
-    // if you want to automatically add an user to da database
-        
-    //    protected override void Seed(Authorization_App.Models.ApplicationDbContext context)
-    //    {
-    //        context.Users.AddOrUpdate(
-    //                    p => p.UserName,
-    //                    new ApplicationUser { UserName = "User Name"}
-    //                );
-    //    }
+        // If you want to automatically add an user to da database
+        protected override void Seed(Authorization_App.Models.ApplicationDbContext context)
+        {
+            var userMgr = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            var appUser = new ApplicationUser()
+            {
+                UserName = "adminUser@authorizationapp.com",
+                Email = "adminUser@authorizationapp.com",
+                EmailConfirmed = true
+            };
+           
+            var IdUserResult = new IdentityResult();
+            IdUserResult = userMgr.Create(appUser, "Sa1b2v3c4m!");
+
+            // If the new "admin" user was successfully created, 
+            // add the "admin" user to the "admin" role. 
+            if (!userMgr.IsInRole(userMgr.FindByEmail("adminUser@authorizationapp.com").Id, "admin"))
+            {
+                IdUserResult = userMgr.AddToRole(userMgr.FindByEmail("adminUser@authorizationapp.com").Id, "admin");
+            }
+
+            context.Users.AddOrUpdate(
+                        p => p.UserName,
+                        appUser
+                    );
+
+        }
     }
 }
